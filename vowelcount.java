9afad8cc2de33d998d0a791f9a35d02fe614fedd
Babu Nagarajan <112308559+baburabbit@users.noreply.github.com>
2023-03-22 14:02:10 +0530

    Update vowelcount.java

--- vowelcount.java
+++ vowelcount.java
@@ -1,24 +1,16 @@
-import java.util.Scanner;
-class vowel
+import java.io.*;
+import java.util.*;
+class vowels
 {
-public static void main(String args[])
-{
-int vcount=0,ccount=0;
-Scanner sc=new Scanner(System.in);
-System.out.println("enter the string:");
-String str=sc.nextLine();
-str=str.toLowerCase();
-for(int i=0;i<str.length();i++)
-{
-if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
-{
-vcount++;
-}
-else
-{
-ccount++;
-}
-}
-System.out.println("Num of vowels:"+vcount);
-}
+	public static void main(String[] args)
+	{
+		String s="worddocument";
+		int vowel;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+			vowel++;
+		}
+		System.out.println("vowels:"+vowel);
+	}
 }
