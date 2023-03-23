e401f2f8c19eba68c394fd17901b15ea61a95fc0
Babu Nagarajan <112308559+baburabbit@users.noreply.github.com>
2023-03-23 10:27:55 +0530

    Add files via upload

---
+++ splcharpattern.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class splchar
+{
+	public static void main(String args[])
+	{
+		int r,i,j;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the no of rows:");		
+		r=sc.nextInt();
+		for(i=1;i<r;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(" %");
+			}
+			System.out.print("\n");
+		}
+	}
+}
+
